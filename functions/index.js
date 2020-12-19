b3afff9a1da74893a69e50936678233ae380b37b
stevshap <stevshap@seas.upenn.edu>
2020-12-19 17:28:17 -0500

    routing

--- functions/index.js
+++ functions/index.js
@@ -6,54 +6,86 @@ admin.initializeApp();
 const db = admin.firestore();
 db.settings({ ignoreUndefinedProperties: true });
 
-//  Create and Deploy Your First Cloud Functions
-//  https://firebase.google.com/docs/functions/write-firebase-functions
+const express = require("express");
+const app = express();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Hoot!");
-});
-
-exports.getUsers = functions.https.onRequest((req, res) => {
+app.get("/users", (req, res) => {
   db.collection("Users")
+    .orderBy("username", "desc")
     .get()
     .then((data) => {
       let users = [];
       data.forEach((doc) => {
-        users.push(doc.data());
+        users.push({
+          userId: doc.id,
+          username: doc.data().username,
+          password: doc.data().password,
+          firstName: doc.data().firstName,
+          lastName: doc.data().lastName,
+          interest1: doc.data().interest1,
+          interest2: doc.data().interest2,
+          interest3: doc.data().interest3,
+          gradYear: doc.data().gradYear,
+          occupation: doc.data().occupation,
+          major: doc.data().major,
+          school: doc.data().school,
+          email: doc.data().email,
+        });
       });
       return res.json(users);
     })
     .catch((err) => console.log(err));
 });
 
-exports.getMentors = functions.https.onRequest((req, res) => {
+app.get("/mentors", (req, res) => {
   db.collection("Mentors")
+  .orderBy('username', 'desc')
     .get()
     .then((data) => {
       let mentors = [];
       data.forEach((doc) => {
-        users.push(doc.data());
+        mentors.push({
+          mentorId: doc.id,
+          username: doc.data().username,
+          password: doc.data().password,
+          firstName: doc.data().firstName,
+          lastName: doc.data().lastName,
+          interest1: doc.data().interest1,
+          interest2: doc.data().interest2,
+          interest3: doc.data().interest3,
+          gradYear: doc.data().gradYear,
+          occupation: doc.data().occupation,
+          major: doc.data().major,
+          school1: doc.data().school1,
+          school2: doc.data().school2,
+          email: doc.data().email,
+        });
       });
-      return res.json(users);
+      return res.json(mentors);
     })
     .catch((err) => console.log(err));
 });
 
-exports.getPairings = functions.https.onRequest((req, res) => {
+app.get("/pairings", (req, res) => {
   db.collection("Pairings")
+  .orderBy('focus', 'desc')
     .get()
     .then((data) => {
       let pairings = [];
       data.forEach((doc) => {
-        users.push(doc.data());
+        pairings.push({
+          pairingId: doc.id,
+          user: doc.data().user,
+          mentor: doc.data().mentor,
+          focus: doc.data().focus,
+        });
       });
-      return res.json(users);
+      return res.json(pairings);
     })
     .catch((err) => console.log(err));
 });
 
-exports.newUser = functions.https.onRequest((req, res) => {
+app.post("/user", (req, res) => {
   const newUser = {
     username: req.body.username,
     password: req.body.password,
@@ -79,46 +111,48 @@ exports.newUser = functions.https.onRequest((req, res) => {
     });
 });
 
-exports.newMentor = functions.https.onRequest((req, res) => {
-    const newMentor = {
-      username: req.body.username,
-      password: req.body.password,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      interest1: req.body.interest1,
-      interest2: req.body.interest2,
-      interest3: req.body.interest3,
-      gradYear: req.body.gradYear,
-      occupation: req.body.occupation,
-      major: req.body.major,
-      school1: req.body.school1,
-      school2: req.body.school2,
-      email: req.body.email,
-    };
-  
-    db.collection("Mentors")
-      .add(newMentor)
-      .then((doc) => {
-        res.json({ message: `document ${doc.id} created successfully` });
-      })
-      .catch((err) => {
-        res.status(500).json({ error: "Error, unsuccessful" });
-      });
-  });
+app.post("/mentor", (req, res) => {
+  const newMentor = {
+    username: req.body.username,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    interest1: req.body.interest1,
+    interest2: req.body.interest2,
+    interest3: req.body.interest3,
+    gradYear: req.body.gradYear,
+    occupation: req.body.occupation,
+    major: req.body.major,
+    school1: req.body.school1,
+    school2: req.body.school2,
+    email: req.body.email,
+  };
 
-  exports.newPairing = functions.https.onRequest((req, res) => {
-    const newUser = {
-      user: req.body.user,
-      mentor: req.body.mentor,
-      focus: req.body.focus
-    };
-  
-    db.collection("Pairings")
-      .add(newUser)
-      .then((doc) => {
-        res.json({ message: `document ${doc.id} created successfully` });
-      })
-      .catch((err) => {
-        res.status(500).json({ error: "Error, unsuccessful" });
-      });
-  });
+  db.collection("Mentors")
+    .add(newMentor)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Error, unsuccessful" });
+    });
+});
+
+app.post("/pairing", (req, res) => {
+  const newPairing = {
+    user: req.body.user,
+    mentor: req.body.mentor,
+    focus: req.body.focus,
+  };
+
+  db.collection("Pairings")
+    .add(newPairing)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Error, unsuccessful" });
+    });
+});
+
+exports.api = functions.https.onRequest(app);
