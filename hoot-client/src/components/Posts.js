4c1d616fe8fe272566a45dcc597081b3ca4c3384
zoupaul <42219007+zoupaul@users.noreply.github.com>
2020-12-21 18:07:22 -0800

    update

--- hoot-client/src/components/Posts.js
+++ hoot-client/src/components/Posts.js
@@ -5,10 +5,19 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
+import Link from "react-router-dom/Link";
 
 const styles = {
   card: {
     display: "flex",
+    marginBottom: 20,
+  },
+  image: {
+    minWidth: 200,
+  },
+  content: {
+    padding: 50,
+    objectFit: "cover",
   },
 };
 export class Posts extends Component {
@@ -18,12 +27,18 @@ export class Posts extends Component {
       posts: { body, commentCount, createdAt, likeCount, userImage, username },
     } = this.props;
     return (
-      <Card>
-        <CardMedia image={userImage} title="Profile pic" />
-        <CardContent>
-        <Typography variant="h5">{username}</Typography>
-        <Typography variant="body2">{createdAt}</Typography>
-        <Typography variant="body1">{body}</Typography>
+      <Card className={classes.card}>
+        <CardMedia
+          image={userImage}
+          title="Profile pic"
+          className={classes.image}
+        />
+        <CardContent class={classes.content}>
+          <Typography variant="h5" color="primary" component={Link} to={`/users/${username}`}>
+            {username} 
+          </Typography>
+          <Typography variant="body2">{createdAt}</Typography>
+          <Typography variant="body1">{body}</Typography>
         </CardContent>
       </Card>
     );
